cf44082dba3819ddb3158a37446f7ef724e1e53c
Richmond Wong <richmondwong@gmail.com>
2018-11-30 16:41:43 +0000

    Creation of w1d5 folder

---
+++ npm_package_creation/index.js
@@ -0,0 +1,7 @@
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.yellow("World");
+console.log(message);
+
+console.log(chalk.greenBright.whiteBright.red("What up bro"))
+console.log(chalk.greenBright.red("What up bro"))
